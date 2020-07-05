b697d6374ed040af856ca4f2646a1d066ac39645
Alex Foreman <foreman.alexander@gmail.com>
2020-07-05 17:01:00 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -10,9 +10,7 @@ def second_challenge
       :patriarch => {}
       :matriarch => {}
       :heroine => {}
-      :heroine_friends => []
-    }
+      :heroine_friends => [] 
+      }
   }
-
-  
 end
